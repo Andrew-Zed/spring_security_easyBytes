dbe3bea8bf880b78ea40810521c90a639c8fbb5c
Andrew-Zed <andrew.awadyke@gmail.com>
2023-09-06 00:12:22 +0100

    Social login implemented with GitHub on PeoplesAPP

---
+++ PeoplesApp/src/main/java/com/peoplesApp/PeoplesAppApplication.java
@@ -0,0 +1,13 @@
+package com.peoplesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplesAppApplication.class, args);
+	}
+
+}
---
+++ PeoplesApp/src/test/java/com/peoplesApp/PeoplesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peoplesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
